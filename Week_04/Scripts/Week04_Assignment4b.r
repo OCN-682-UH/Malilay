af370a4afb60c7ff0b5ddacae8cc9c9497ab662b
Ally Malilay <amalilay@hawaii.edu>
2025-09-23 16:12:51 -1000

    Update to script and output for Assignment 4b

--- Week_04/Scripts/Week04_Assignment4b.r
+++ Week_04/Scripts/Week04_Assignment4b.r
@@ -25,7 +25,7 @@ ChemData_pivot <- ChemData_clean %>%
   pivot_longer(cols = Temp_in:percent_sgd, #choose columns to pivot 
                names_to = "Variables", #names of new columns
                values_to = "Values")  %>% #name of new column holding all variable values
-  group_by(Variables, Site, Zone) %>%
+  group_by(Variables, Site, Season, Zone, Tide, Time) %>%
   summarise(mean_vals= mean(Values, na.rm=TRUE)) %>%
   pivot_wider(names_from=Variables,
             values_from= mean_vals) %>% ###want to widen the data to make it look better
@@ -33,20 +33,21 @@ ChemData_pivot <- ChemData_clean %>%
 
 ### Create a plot
 p <- ggplot(data = ChemData_pivot,
-               mapping = aes(x = percent_sgd, y = Silicate, color = Zone)) +
-         geom_point() + #show increasing correlation
+               mapping = aes(x = percent_sgd, y = Silicate)) +
+         geom_bin_2d() + #show increasing correlation with higher silicate lvls, higher % groundwater discharge
+  facet_wrap(~Tide, scales = "free") + #display plots based on tide
   labs(title = "Average submarine groundwater discharge vs average silicate", 
-       x = "% of Submarine Groundwater Discharge", y = "Silicate",
-       color = "Zone",  #differentiate color by zone
+       subtitle = "High tide vs low tide",
+       x = "Submarine Groundwater Discharge (%)", y = "Silicate (umol/L)",
+       shape = "Tide",  #differentiate color by zone
        caption = "Source: Dr. Nyssa Silbiger") +
-  scale_color_manual(values = beyonce_palette(127)) + #add beyonce color palette
-  theme_dark() + #change theme to dark to highlight plot points
+  theme_bw() + #change theme to dark to highlight plot points
   theme(axis.title = element_text(size = 12), #change size of axis titles
         title = element_text(size = 10, face = "bold"), #increase title size and make bold
         plot.subtitle = element_text(face = "plain"), #subtitle plain text instead of bold
         plot.caption = element_text(face = "plain")) #caption plain text instead of bold#add geometry
 
-
+p
 ### Export plot
 ggsave(filename = here("Week_04", "Output", #where to save it
                         "Assignment 4b: Average submarine groundwater discharge vs average silicate.png"), #name to save it
